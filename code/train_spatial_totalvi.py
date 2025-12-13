cd85b07a7e6d08190919ad8bf829faef4b1fae3f
mo <yanngajwbdis@gmail.com>
2025-12-13 11:17:45 -0800

    Backup(data/ was not added as of size?)

--- code/train_spatial_totalvi.py
+++ code/train_spatial_totalvi.py
@@ -1,63 +1,60 @@
 import sys
 sys.dont_write_bytecode = True
-
-sys.path.insert(0, "/ubc/cs/research/beaver/projects/carlos/spatial_totalvi/model/src/")
+dependency_path = ""
+sys.path.insert(0, f"{dependency_path}model/src/")
 from scvi.model._totalvi import TOTALVI
+import os
+os.environ["MKL_VERBOSE"] = "0"
+os.environ["MKL_DISABLE_FAST_MM"] = "1"
+import warnings
+warnings.filterwarnings("ignore", message="Intel MKL")
+warnings.filterwarnings("ignore", category=FutureWarning)
 
-import torch
 import numpy as np
 import mudata as md
 import scanpy as sc
 from graph.graph import prep_graph_splits
+import muon
 
-path_to_mdata = "/ubc/cs/research/beaver/projects/carlos/spatial_totalvi/data/tonsil/tonsil_pp_svg.h5mu"
-path_to_full_graph = "/ubc/cs/research/beaver/projects/carlos/spatial_totalvi/data/graph/graph_k5_conn.pt"
-path_to_split_graph = "/ubc/cs/research/beaver/projects/carlos/spatial_totalvi/data/graph/k5_conn_train_val_subgraphs.pt"
-path_to_model = "/ubc/cs/research/beaver/projects/carlos/spatial_totalvi/data/trained_models/conn/totalvi_k5_1layer_SGC_k1.pt"
 
-#############################
-### Load data & subgraphs ###
-#############################
-mdata = md.read_h5mu(path_to_mdata)
-print(mdata)
+choices = [
+    "6_conn", "6_sim_gau", "6_sim_inv",
+    "10_conn", "10_sim_gau", "10_sim_inv",
+    "30_conn", "30_sim_gau", "30_sim_inv",
+    "50_conn", "50_sim_gau", "50_sim_inv"
+]
+for choice in choices:
 
-subgraphs = prep_graph_splits(
-    mdata,
-    path_to_graph=path_to_full_graph,
-    path_to_save_graph_splits=path_to_split_graph,
-    train_split=0.75
-)
+    path_to_mdata = f"{dependency_path}data/tonsil/tonsil_pp_svg2.h5mu"
+    path_to_full_graph = f"{dependency_path}data/graph/graph_k{choice}.pt"
+    path_to_split_graph = f"{dependency_path}data/graph/k{choice}_train_val_subgraphs.pt"
+    path_to_model = f"{dependency_path}data/trained_models/conn/totalvi_k{choice}_1layer_SGC_k1.pt"
 
-######################
-### Set up totalVI ###
-######################
-TOTALVI.setup_mudata(
-    mdata,
-    rna_layer=None,
-    protein_layer=None,
-    modalities ={
-        "rna_layer": "RNA",
-        "protein_layer": "Protein"
-    }
-)
+    #############################
+    ### Load data & subgraphs ###
+    #############################
+    mdata = muon.read_h5mu(path_to_mdata)
+   # print(mdata)
+    print(path_to_full_graph)
+   # print(path_to_split_graph)
 
-model = TOTALVI(
-    mdata,
-    path_to_graphs=path_to_split_graph,
-    graph_n_layers=1,
-    graph_conv_type="SGC",
-    graph_norm_type="layer",
-    graph_act_type="elu",
-    graph_sgc_Kparam=1
-)
-print(model.module.gnn)
+    subgraphs = prep_graph_splits(
+        mdata,
+        path_to_graph=path_to_full_graph,
+        path_to_save_graph_splits=path_to_split_graph,
+        train_split=0.75
+    )
 
-####################
-### Train & save ###
-####################
-model.train(
-    max_epochs=400,
-    batch_size=len(subgraphs["train_indices"]),
-    external_indexing=[subgraphs["train_indices"], subgraphs["val_indices"]]
-)
-model.save(path_to_model)
+    #spatial_totalvi/data/tonsil/tonsil_pp_svg2.h5mu
+
+    ######################
+    ### Set up totalVI ###
+    ######################
+    TOTALVI.setup_mudata(
+        mdata,
+        rna_layer=None,
+        protein_layer=None,
+        modalities ={
+            "rna_layer": "RNA",
+            "protein_layer": "Protein"
+ 
